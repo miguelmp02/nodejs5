9cf23bced0b0346425993c298008b98844c632e0
Miguel <miguel.merinoplaza@gmail.com>
2023-12-11 11:38:53 +0100

    login

---
+++ myapp/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Login" });
+});
+
+
+
+router.post('/login', function(req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    res.redirect('/home');
+  } else {
+    res.render("/error");
+  }
+});
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+module.exports = router;
